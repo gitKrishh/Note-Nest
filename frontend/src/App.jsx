de84ed6043158813c18cc73cd846ea0b1083c12c
gitkrishh <msgkrish192@gmail.com>
2025-07-20 18:32:54 +0530

    added the logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom';
@@ -8,6 +6,7 @@ import Home from './pages/Home';
 import Feedback from './pages/About';
 import { Outlet } from 'react-router-dom'
 import Navs from './components/Navs'
+
 
 function App() {
   const activeStyle = {
